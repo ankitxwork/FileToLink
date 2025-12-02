922362b3d918a1fd8eaec1a21491525e20012ce7
ankitxwork <ankitxworking@gmail.com>
2025-12-02 19:31:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-API_ID = YOUR_API_ID 
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-LOG_CHANNEL_ID = YOUR_CHANNEL_ID
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
+BASE_URL = os.getenv("BASE_URL")
