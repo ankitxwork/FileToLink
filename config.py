89f8f5ed013729f832419d3d7a44b3e52014f100
ankitxwork <ankitxworking@gmail.com>
2025-12-02 17:43:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
-import os
-
-# 1. FIX: Load numerical IDs and cast them to integers
-API_ID = int(os.environ.get("API_ID", 0))
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 0))
-
-# 2. FIX: Load string-based secrets
-API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
-
-# BASE_URL is also required
-BASE_URL = os.environ.get("BASE_URL", "http://localhost:8000")
+API_ID = YOUR_API_ID 
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+LOG_CHANNEL_ID = YOUR_CHANNEL_ID
